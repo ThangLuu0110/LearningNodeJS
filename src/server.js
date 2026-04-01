e91dea937ee695f3405d0025dc8823c1dda4faa0
Lưu Mạnh Thắng <98244656+ThangLuu0110@users.noreply.github.com>
2026-04-01 22:37:55 +0700

    feat: update folder struction mvc

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const path = require('path')
 require('dotenv').config();
 
-console.log(">>> check env: ", process.env);
-
 const app = express()
 const port = process.env.PORT || 3000
 const hostname = process.env.HOST_NAME || 'localhost'
@@ -12,8 +10,11 @@ const hostname = process.env.HOST_NAME || 'localhost'
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+//config static files
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello The Whole New World!')
 })
 
 app.get('/abc', (req, res) => {
